93a1cac5e0921aaba51027a557e9362ab9edbd3b
Md Jawed <mdjawedh22@gmail.com>
2023-04-10 23:56:22 +0530

    cors update

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express=require("express");
 const { connection } = require("./config/db");
 const { userRouter } = require("./routes/users.routes");
+const cors=require("cors")
 require("dotenv").config();
 
 
@@ -10,6 +11,7 @@ const app=express();
 app.get("/",(req,res)=>{
     res.send("welcome Home")
 })
+app.use(cors())
 app.use(express.json())
 
 app.use("/user",userRouter)
